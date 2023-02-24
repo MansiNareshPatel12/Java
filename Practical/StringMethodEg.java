57ac36e3ca39350a64afd70045ff8051598d5df7
MansiNareshPatel12 <124339934+MansiNareshPatel12@users.noreply.github.com>
2023-02-24 20:38:51 +0530

    Add files via upload

---
+++ Practical/StringMethodEg.java
@@ -0,0 +1,24 @@
+package my;
+
+public class StringMethodEg {
+
+	public static void main(String[] args) {
+		String a = "Anudip Foundation";
+		System.out.println(a.toUpperCase());
+		System.out.println(a.toLowerCase());
+		System.out.println(a.startsWith("Anu"));
+		System.out.println(a.endsWith("tion"));
+		System.out.println(a.charAt(8));
+		System.out.println(a.length());
+		System.out.println(a.replace("Anudip","Anudeep"));
+		System.out.println(a);
+		
+		int num = 55;
+		String b = String.valueOf(num);//"55"
+		System.out.println(b+55);
+		
+		
+
+	}
+
+}
